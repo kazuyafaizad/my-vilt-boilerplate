8556b5efb18e57cb2a46645f029792dcb983da5b
kazuyafaizad <kazuyafaizad@gmail.com>
2023-06-18 04:43:06 +0800

    stats

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,7 +16,7 @@ import needWantSavingStats from '@/Components/NeedWantSavingStats.vue';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <need-want-saving-stats :kyuuryou="4200"></need-want-saving-stats>
+                <need-want-saving-stats :kyuuryou="7000"></need-want-saving-stats>
 
                 <div class="bg-base-100 overflow-hidden shadow-xl sm:rounded-lg">
 
